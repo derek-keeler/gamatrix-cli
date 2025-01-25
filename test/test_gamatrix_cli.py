08b996a73da5c89618da42c5f8da273260de86d0
Derek Keeler <34773432+derek-keeler@users.noreply.github.com>
2025-01-25 07:03:44 -0800

    Add a first test to ensure commands are present

--- test/test_gamatrix_cli.py
+++ test/test_gamatrix_cli.py
@@ -1,5 +1,16 @@
+from click.testing import CliRunner
+
+from gamatrixcli.__main__ import gcli
 
 
 def test_get_r_done():
     assert True
 
+
+def test_commands_exist():
+    runner = CliRunner()
+    result = runner.invoke(gcli, ["--help"])
+    assert result.exit_code == 0
+    assert "add-db" in result.output
+    assert "show" in result.output
+    assert "compare" in result.output
